750e127e8c6f19ff74c64526e3b86f2ef411d33c
Shawn1515 <187825911+Shawn1515@users.noreply.github.com>
2026-04-12 20:49:01 -0400

    added sensor-registry, alert-service services
    added sensor-registry and alert-service services to compose.yml and tested (healthy)

--- alert-service/server.js
+++ alert-service/server.js
@@ -10,11 +10,7 @@ const PORT = process.env.PORT || 3000;
 const { Pool } = pkg;
 
 const db = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 5432
+    connectionString: process.env.DATABASE_URL
 });
 
 const redis = createClient({
