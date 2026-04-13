58435449f84ae090f341f40bf5efee91acfebbce
namdavid2904 <89288986+namdavid2904@users.noreply.github.com>
2026-04-12 22:25:24 -0400

    Created POST /dashboard endpoint to submit sensor readings

--- dashboard-api/server.js
+++ dashboard-api/server.js
@@ -1,14 +1,33 @@
 import express from 'express'
 import redis from 'redis'
+import { randomUUID } from 'crypto'
+import { Pool } from 'pg'
 
 const app = express()
 const port = 3000
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
 
 app.use(express.json())
 
 app.get('/health', (_req, res) => {
     res.json({
         service: 'Dashboard API'
+    })
+})
+
+app.post('/dashboard', async (req, res) => {
+    const { sensor_id, timestamp, temperature, pressure, humidity } = req.body;
+    const reading_id = randomUUID();
+    await pool.query(
+        `INSERT INTO sensor_readings (reading_id, timestamp, sensor_id, temperature, pressure, humidity)
+        VALUES ($1, $2, $3, $4, $5, $6)`,
+        [reading_id, timestamp, sensor_id, temperature, pressure, humidity]
+    )
+    return res.status(200).json({
+        status: 'success',
+        reading_id
     })
 })
 
