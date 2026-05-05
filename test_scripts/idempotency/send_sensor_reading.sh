14a21478b1302c9158c982786b5f78ba5b93765a
CELERY\natec <natechen314@gmail.com>
2026-05-05 01:07:55 -0400

    Completed copy-and-paste tests

--- test_scripts/idempotency/send_sensor_reading.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST http://ingestion:3001/sensor \
-  -H "Content-Type: application/json" \
-  -d '{"sensor_id": "unregistered-999", "timestamp": "2025-04-24T12:00:00Z", "temperature": 25, "pressure": 1010, "humidity": 50}'
